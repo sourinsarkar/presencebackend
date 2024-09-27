8cedc18059e1cfe764777b6c584bed303bafc93d
Sourin <iamsourinsarkar@gmail.com>
2024-09-27 14:41:46 +0530

    Created Platform controlers

--- src/controllers/platformController.ts
+++ src/controllers/platformController.ts
@@ -2,4 +2,13 @@ import { Request, Response } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import { createPlatform, getPlatorms } from "../services/platformService";
 
-export const addPlatform = asyncHandler(async(req: Request, res:))
+export const addPlatform = asyncHandler(async(req: Request, res: Response) => {
+    const { name } = req.body;
+    const platform = await createPlatform(name);
+    res.status(201).json(platform);
+});
+
+export const getAllPlatforms = asyncHandler(async(req: Request, res: Response) => {
+    const platforms = await getPlatorms();
+    res.status(200).json(platforms);
+});
